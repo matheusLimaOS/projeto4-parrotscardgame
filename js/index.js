764a4f4d0c72c8994235ca493f8b045679940bf4
matheussouzalima <matholaslima@gmail.com>
2022-08-18 16:09:16 -0300

    inserção cartas pelo js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,31 @@
-let numCartas = prompt("Escolha um número par de cartas entre 4 e 14:");
 let arrayCartas = [];
-let i=0;
+let jaExisteCartaVirada = false;
+let paresDescobertos = 0;
+let numCartas;
+let movimentos = 0;
 
-while(numCartas < 4 || numCartas > 14 || numCartas%2!==0){
-    numCartas = prompt("Número invalido! Por favor Insira um numero par entre 4 e 14!");
-}
-
-
-function criarCartas(numCartas){
-    let cards = document.querySelector(".cards");
-    let card = document.createElement("li");
-    let img = document.createElement("img");
-    card.classList.add("card");
-    img.setAttribute("src","./img/front 5.png")
-    card.appendChild(img);
-
-    for(i=0;i<numCartas;i++){
-        card = document.createElement("li");
-        img = document.createElement("img");
-        caard.classList.add("card");
-        img.setAttribute("src","./img/front 5.png")
-        card.appendChild(img);
-        cards.appendChild(card);
+function inicio(){
+    let i=0;
+    movimentos = 0;
+    numCartas = prompt("Escolha um número par de cartas entre 4 e 14:");
+    jaExisteCartaVirada = false;
+    paresDescobertos = 0;
+    arrayCartas = [];
+    while(numCartas < 4 || numCartas > 14 || numCartas%2!==0){
+        numCartas = prompt("Número invalido! Por favor Insira um numero par entre 4 e 14!");
     }
+
+    for(i=0;i<numCartas/2;i++){
+        arrayCartas.push(i);
+        arrayCartas.push(i);
+    }
+
+    arrayCartas.sort(()=>{
+        return Math.random() - 0.5;
+    })
+
+    criarCartas(numCartas,arrayCartas);
 }
+
+
+inicio();
