5d7852f5d968f3bf27b1871a4084215535883962
matheussouzalima <matholaslima@gmail.com>
2022-08-19 17:09:07 -0300

    Correção bug virar carta

--- js/index.js
+++ js/index.js
@@ -6,25 +6,28 @@ let movimentos = 0;
 let dataInicio;
 let tempo;
 
-function inicio(){
+async function inicio(){
     let i=0;
     let cards = document.querySelector('.cards');
+    let clockIcon = document.createElement('ion-icon');
+    let tempoRelogio = document.createElement('p');
     let temp = document.createElement('div');
+    clockIcon.setAttribute("name","time-outline");
     temp.classList.add('temporizador');
+    temp.appendChild(clockIcon);
+    temp.appendChild(tempoRelogio);
     cards.appendChild(temp);
 
     movimentos = 0;
-    numCartas = prompt("Escolha um número par de cartas entre 4 e 14:");
     jaExisteCartaVirada = false;
     paresDescobertos = 0;
     arrayCartas = [];
-    dataInicio = new Date();
 
-    comecaTemporizador();
-
+    numCartas = prompt("Escolha um número par de cartas entre 4 e 14:");
     while(numCartas < 4 || numCartas > 14 || numCartas%2!==0){
         numCartas = prompt("Número invalido! Por favor Insira um numero par entre 4 e 14!");
     }
+
 
     for(i=0;i<numCartas/2;i++){
         arrayCartas.push(i);
@@ -35,9 +38,11 @@ function inicio(){
         return Math.random() - 0.5;
     })
 
-    criarCartas(numCartas,arrayCartas);
+    await criarCartas(numCartas,arrayCartas);
+    dataInicio = new Date();
+    comecaTemporizador();
 }
-function criarCartas(numCartas){
+async function criarCartas(numCartas){
     let cards = document.querySelector(".cards");
     let card;
     let img;
@@ -45,12 +50,16 @@ function criarCartas(numCartas){
 
     for(i=0;i<numCartas;i++){
         card = document.createElement("li");
+
         img = document.createElement("img");
         img2 = document.createElement("img");
         card.classList.add("card");
+        
+        img.innerHTML='Frente';
+        img2.innerHTML='Verso';
 
-        img.setAttribute("class","front-face face");
-        img2.setAttribute("class","back-face face");
+        img.classList.add(["front-face"],["face"]);
+        img2.classList.add(["back-face"],["face"]);
         img.setAttribute("src","./img/front 5.png");
         img2.setAttribute("src","/img/"+arrayCartas[i]+".gif");
 
@@ -90,6 +99,10 @@ async function virarCarta(card){
                     let reinicio;
                     while(true){
                         reinicio = prompt(`Parabéns, você ganhou em ${movimentos} jogadas!. E com o tempo de ${time[0]} minutos e ${time[1]} segundos!\nDeseja jogar novamente? (sim ou não)`);
+                        
+                        if(reinicio===null){
+                            continue;
+                        }
 
                         if(reinicio.toLowerCase() === 'sim' || reinicio.toLowerCase() === 'não'){
                             break;
@@ -119,26 +132,26 @@ async function virarCarta(card){
 }
 function comecaTemporizador(){
     setInterval(function(){
-  	    let segundos = 1000;
-        let minutos = segundos * 60;
-        let hora = minutos*60;
-        
-        var atual = new Date();
-        
-        var diferenca = atual - dataInicio;
-        
-        let minutos1 = Math.floor((diferenca % hora) / minutos);
-        let segundos1 = Math.floor((diferenca % minutos) / segundos);
-        if(minutos1 < 10){
-            minutos1 = '0' + minutos1;
-        }
-        if(segundos1 < 10){
-            segundos1 = '0' + segundos1;
-        }
+      let segundos = 1000;
+      let minutos = segundos * 60;
+      let hora = minutos*60;
+      
+      var atual = new Date();
+      
+      var diferenca = atual - dataInicio;
+      
+      let minutos1 = Math.floor((diferenca % hora) / minutos);
+      let segundos1 = Math.floor((diferenca % minutos) / segundos);
+      if(minutos1 < 10){
+          minutos1 = '0' + minutos1;
+      }
+      if(segundos1 < 10){
+          segundos1 = '0' + segundos1;
+      }
 
-        tempo = minutos1 + ":" + segundos1;
-        document.querySelector('.temporizador').innerHTML = tempo;
-    },1000);
+      tempo = minutos1 + ":" + segundos1;
+      document.querySelector('.temporizador p').innerText = tempo;
+  },1000);
 }
 
 inicio();
